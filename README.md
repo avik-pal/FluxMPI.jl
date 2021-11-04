4240229bbce363e3d2d292aa28a9dc26ac1d1219
Avik Pal <avikpal@iitk.ac.in>
2021-11-04 17:10:56 -0400

    Some working code

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # FluxMPI.jl
-MultiGPU / MultiNode Training of Flux Models
+
+Project Status: **Experimental**
+
+Data Parallel Training of Flux Models. This is mostly an experimental package that I am using for my research projects. Additionally, this is primarily geared towards MultiGPU training and not multiple node training.
+
+## Quick Start
+
+```julia
+using Flux, FluxMPI, MPI, Zygote
+
+MPI.Init()
+
+model = Chain(Dense(1, 2, tanh), Dense(2, 1))
+
+model_dp = DataParallelFluxModel(model, [0 for _ in 1:MPI.Comm_size(MPI.COMM_WORLD)])
+
+ps = Flux.params(model_dp)
+
+x = rand(1, 10) |> gpu
+
+gs = Zygote.gradient(() -> sum(model_dp(x)), Flux.params(model_dp))
+```
+
+Run the code using `mpiexecjl -n 3 julia --project=. <filename>.jl`.
+
+
+## TODOs
+
+- [ ] Can we check if MPI is CUDA aware?
---
+++ src/FluxMPI.jl
@@ -0,0 +1,99 @@
+module FluxMPI
+
+using Flux, CUDA, MPI, Zygote
+
+struct DataParallelFluxModel{M}
+    model::M
+    device_count::Int
+end
+
+struct DataParallelParamsWrapper{B,S,L,P}
+    buffer::B
+    sizes::S
+    lengths::L
+    params::P
+end
+
+Flux.@functor DataParallelFluxModel
+
+Flux.trainable(dp::DataParallelFluxModel) = Flux.trainable(dp.model)
+
+function Flux.params(dp::DataParallelFluxModel)
+    ps = Flux.params(dp.model)
+    _buffer = zero.(ps)
+    sizes = size.(_buffer)
+    lengths = length.(_buffer)
+    buffer = vcat(vec.(_buffer)...)
+    return DataParallelParamsWrapper(buffer, sizes, lengths, ps)
+end
+
+function DataParallelFluxModel(model, gpu_devices::Vector{Int} = [])
+    device_count = length(gpu_devices)
+
+    if CUDA.functional()
+        @assert device_count > 0
+
+        comm = MPI.COMM_WORLD
+        rank = MPI.Comm_rank(comm)
+
+        gpu_id = gpu_devices[rank+1]
+
+        @info "Rank $rank: Using GPU $gpu_id"
+        CUDA.device!(gpu_id)
+
+        model = model |> gpu
+    end
+
+    return DataParallelFluxModel(model, device_count)
+end
+
+(dp::DataParallelFluxModel)(args...) = dp.model(args...)
+
+
+function flatten_grads(ps::DataParallelParamsWrapper, gs::Zygote.Grads)
+    idx = 1
+    for (i, p) in enumerate(gs.params)
+        l = ps.lengths[i]
+        ps.buffer[idx:idx+l-1] .= vec(gs[p])
+        idx += l
+    end
+    return ps.buffer
+end
+
+function unflatten_grads!(
+    gs::Zygote.Grads,
+    ps::DataParallelParamsWrapper,
+    gs_flattened::AbstractArray,
+)
+    idx = 1
+    for (i, p) in enumerate(gs.params)
+        l = ps.lengths[i]
+        gs[p] = reshape(gs_flattened[idx:idx+l-1], ps.sizes[i])
+        idx += l
+    end
+    return gs
+end
+
+
+function Zygote.gradient(func, ps::DataParallelParamsWrapper)
+    comm = MPI.COMM_WORLD
+    size = MPI.Comm_size(comm)
+
+    gs = Zygote.gradient(func, ps.params)
+
+    # CUDA aware MPI not working locally :(
+    gs_flattened = flatten_grads(ps, gs) |> cpu
+
+    MPI.Allreduce!(gs_flattened, +, comm)
+
+    if CUDA.functional()
+        gs_flattened = gs_flattened |> gpu
+    end
+
+    return unflatten_grads!(gs, ps, gs_flattened ./ size)
+end
+
+
+export DataParallelFluxModel
+
+end
